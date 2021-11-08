c92888a06d72f88941f29cd60a6f02fffa5b400b
aaron.kannangara <aaron.kannangara@indicia.nl>
2021-11-08 21:10:38 +0100

    Store all project issues into models and save as csv files. different jira types are also stored as csv files

--- ProcessMining/Jira/JiraConfig.py
+++ ProcessMining/Jira/JiraConfig.py
@@ -5,3 +5,17 @@ Jira = {
 }
 
 debug=False
+
+DataStoreFolder = "C:\_thesis\DataStore"
+
+GraphVizLocation = "C:\Program Files\Graphviz\bin"
+
+ImageStorage = {
+    "base":"C:\_thesis\Images",
+    "alphaMiner":"Alphaminer.png",
+    "inductiveMiner":"InductiveMiner.png",
+    "heuristicsMiner":"HeuristicsMiner.png",
+    "dfg":"DFG.png"
+}
+
+JiraExpectedProcess = "C:\_thesis\jiraProcess.csv"
